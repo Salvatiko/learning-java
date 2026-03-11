c8adffb5e5dcc27ec6c66ef2be6bdd99736d1eff
Lucas Salvático <l.salvatiko@gmail.com>
2026-03-11 20:56:26 -0300

    Add files via upload

---
+++ POO/Soma.java
@@ -0,0 +1,18 @@
+public class Soma{
+
+	public static void main (String arg[]){
+	
+		int a = 10;
+		int b = 6;
+		calc(a,b);
+	
+	}
+	
+	public static void calc(int x, int y){
+	
+		int c = x+y;
+		System.out.println("\nSOMA: " + c);
+	
+	}
+
+}
