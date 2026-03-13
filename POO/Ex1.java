cf056e1cf843429a998f3d954d3ac57a424938ba
Lucas Salvático <l.salvatiko@gmail.com>
2026-03-13 17:51:37 -0300

    Add files via upload

--- POO/Ex1.java
+++ POO/Ex1.java
@@ -1,10 +1,12 @@
-public class Ex1{
-
-	public static void main(String args[]){
+public class Ex1 {
+    	public static void main(String args[]){
 
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		soma(a,b);
+        sub(a,b);
+        mult(a,b);
+        div(a,b);
 
 	}
 	
@@ -12,5 +14,14 @@ public class Ex1{
 		int z = x + y;
 		System.out.println("Soma: " + z);	
 	}
-	
+    public static void sub(int v, int w){
+		System.out.println("Subtracao " + (v+w));	
+	}
+    public static void mult(int c, int d){
+		int g = c * d;
+		System.out.println("Multiplicacao: " + g);	
+	}
+    public static void div(int e, int f){
+		System.out.println("Divisao: " + (e/f));	
+	}
 }
