6b073e1c877b240fc05c1ba1f306fa8724701f5a
Lucas Salvático <l.salvatiko@gmail.com>
2026-03-25 21:01:24 -0300

    Add files via upload

--- POO/Pessoa.java
+++ POO/Pessoa.java
@@ -1,30 +1,23 @@
 public class Pessoa{
 
-	static String nome = "";
-	static int cpf = 0;
+	private String nome = "";
+	private int cpf = 0;
 	
-	public static void main(String[] args){
-		
-		Pessoa p1; //declaraçao
-		p1 = new Pessoa(); //instanciaçao
-		
-			/*new serve para reservar um espaço 
-			e Pessoa() conhecido como metodo construtor e,
-			com o SO, calcula o espaço a ser utilizado*/
-		
-		entDados("Jesus", 10);
-		saiDados();
+	public String getNome(){
+		return nome;
+	}
+	public int getCpf(){
+		return cpf;
+	}
+	
+	public void entDados(String nome, int cpf){
+	
+	this.nome = nome;
+	this.cpf = cpf;
 	
 	}
 	
-	public static void entDados(String n, int doc){
-	
-	nome = n;
-	cpf = doc;
-	
-	}
-	
-	public static void saiDados(){
+	public void saiDados(){
 	
 	System.out.println("\n Nome: "+ nome);
 	System.out.println("\n CPF: "+ cpf);
---
+++ POO/TstPes.java
@@ -0,0 +1,10 @@
+public class TstPes{	
+	public static void main(String[] args){
+		
+		Pessoa p1;
+		p1 = new Pessoa(); 
+		p1.entDados("Jesus", 10);
+		p1.saiDados();
+	
+	}
+}
